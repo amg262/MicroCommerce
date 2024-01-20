8d3f32af4cf83b60c5f81ee2676231570615cd87
Andrew Gunn <andrewgunn31@gmail.com>
2024-01-19 20:30:55 -0600

    order management admin page working

--- Micro.Services.OrderAPI/Controllers/OrderAPIController.cs
+++ Micro.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -7,6 +7,7 @@ using Micro.Services.OrderAPI.Service.IService;
 using Micro.Services.OrderAPI.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Stripe;
 using Stripe.Checkout;
 
@@ -35,6 +36,53 @@ public class OrderAPIController : ControllerBase
 	}
 
 	[Authorize]
+	[HttpGet("GetOrders")]
+	public ResponseDto? Get(string? userId = "")
+	{
+		try
+		{
+			IEnumerable<OrderHeader> objList;
+			if (User.IsInRole(SD.RoleAdmin))
+			{
+				objList = _db.OrderHeaders.Include(u => u.OrderDetails).OrderByDescending(u => u.OrderHeaderId)
+					.ToList();
+			}
+			else
+			{
+				objList = _db.OrderHeaders.Include(u => u.OrderDetails).Where(u => u.UserId == userId)
+					.OrderByDescending(u => u.OrderHeaderId).ToList();
+			}
+
+			_response.Result = _mapper.Map<IEnumerable<OrderHeaderDto>>(objList);
+		}
+		catch (Exception ex)
+		{
+			_response.IsSuccess = false;
+			_response.Message = ex.Message;
+		}
+
+		return _response;
+	}
+
+	[Authorize]
+	[HttpGet("GetOrder/{id:int}")]
+	public ResponseDto? Get(int id)
+	{
+		try
+		{
+			OrderHeader orderHeader = _db.OrderHeaders.Include(u => u.OrderDetails).First(u => u.OrderHeaderId == id);
+			_response.Result = _mapper.Map<OrderHeaderDto>(orderHeader);
+		}
+		catch (Exception ex)
+		{
+			_response.IsSuccess = false;
+			_response.Message = ex.Message;
+		}
+
+		return _response;
+	}
+
+	[Authorize]
 	[HttpPost("CreateOrder")]
 	public async Task<ResponseDto> CreateOrder([FromBody] CartDto cartDto)
 	{
@@ -55,6 +103,40 @@ public class OrderAPIController : ControllerBase
 		{
 			_response.IsSuccess = false;
 			_response.Message = ex.Message;
+		}
+
+		return _response;
+	}
+
+	[Authorize]
+	[HttpPost("UpdateOrderStatus/{orderId:int}")]
+	public async Task<ResponseDto> UpdateOrderStatus(int orderId, [FromBody] string newStatus)
+	{
+		try
+		{
+			OrderHeader orderHeader = _db.OrderHeaders.First(u => u.OrderHeaderId == orderId);
+			if (orderHeader != null)
+			{
+				if (newStatus == SD.Status_Cancelled)
+				{
+					//we will give refund
+					var options = new RefundCreateOptions
+					{
+						Reason = RefundReasons.RequestedByCustomer,
+						PaymentIntent = orderHeader.PaymentIntentId
+					};
+
+					var service = new RefundService();
+					Refund refund = await service.CreateAsync(options);
+				}
+
+				orderHeader.Status = newStatus;
+				await _db.SaveChangesAsync();
+			}
+		}
+		catch (Exception ex)
+		{
+			_response.IsSuccess = false;
 		}
 
 		return _response;
