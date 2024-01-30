f22c5d788d019168023f0595c8bacd2fc33700cf
Andrew Gunn <andrewgunn31@gmail.com>
2024-01-30 11:19:50 -0600

    adding all cases and organized the enum integer value of status code

--- Micro.Web/Service/BaseService.cs
+++ Micro.Web/Service/BaseService.cs
@@ -113,14 +113,104 @@ public class BaseService : IBaseService
 			// Handle different HTTP status codes
 			switch (apiResponse.StatusCode)
 			{
-				case HttpStatusCode.NotFound:
-					return new ResponseDto {Message = "Not Found", IsSuccess = false};
-				case HttpStatusCode.Forbidden:
-					return new ResponseDto {Message = "Forbidden Access", IsSuccess = false};
+				case HttpStatusCode.PermanentRedirect:
+					return new ResponseDto {Message = "Permanent Redirect", IsSuccess = false};
+				case HttpStatusCode.BadRequest:
+					return new ResponseDto {Message = "Bad Request", IsSuccess = false};
 				case HttpStatusCode.Unauthorized:
 					return new ResponseDto {Message = "Unauthorized", IsSuccess = false};
+				case HttpStatusCode.PaymentRequired:
+					return new ResponseDto {Message = "Payment Required", IsSuccess = false};
+				case HttpStatusCode.Forbidden:
+					return new ResponseDto {Message = "Forbidden Access", IsSuccess = false};
+				case HttpStatusCode.NotFound:
+					return new ResponseDto {Message = "Not Found", IsSuccess = false};
+				case HttpStatusCode.MethodNotAllowed:
+					return new ResponseDto {Message = "Method Not Allowed", IsSuccess = false};
+				case HttpStatusCode.NotAcceptable:
+					return new ResponseDto {Message = "Not Acceptable", IsSuccess = false};
+				case HttpStatusCode.ProxyAuthenticationRequired:
+					return new ResponseDto {Message = "Proxy Authentication Required", IsSuccess = false};
+				case HttpStatusCode.RequestTimeout:
+					return new ResponseDto {Message = "Request Timeout", IsSuccess = false};
+				case HttpStatusCode.Conflict:
+					return new ResponseDto {Message = "Conflict", IsSuccess = false};
+				case HttpStatusCode.Gone:
+					return new ResponseDto {Message = "Gone", IsSuccess = false};
+				case HttpStatusCode.LengthRequired:
+					return new ResponseDto {Message = "Length Required", IsSuccess = false};
+				case HttpStatusCode.PreconditionFailed:
+					return new ResponseDto {Message = "Precondition Failed", IsSuccess = false};
+				case HttpStatusCode.RequestEntityTooLarge:
+					return new ResponseDto {Message = "Request Entity Too Large", IsSuccess = false};
+				case HttpStatusCode.RequestUriTooLong:
+					return new ResponseDto {Message = "Request-URI Too Long", IsSuccess = false};
+				case HttpStatusCode.UnsupportedMediaType:
+					return new ResponseDto {Message = "Unsupported Media Type", IsSuccess = false};
+				case HttpStatusCode.RequestedRangeNotSatisfiable:
+					return new ResponseDto {Message = "Requested Range Not Satisfiable", IsSuccess = false};
+				case HttpStatusCode.ExpectationFailed:
+					return new ResponseDto {Message = "Expectation Failed", IsSuccess = false};
+				case HttpStatusCode.MisdirectedRequest:
+					return new ResponseDto {Message = "Misdirected Request", IsSuccess = false};
+				case HttpStatusCode.UnprocessableEntity:
+					return new ResponseDto {Message = "Unprocessable Entity", IsSuccess = false};
+				case HttpStatusCode.Locked:
+					return new ResponseDto {Message = "Locked", IsSuccess = false};
+				case HttpStatusCode.FailedDependency:
+					return new ResponseDto {Message = "Failed Dependency", IsSuccess = false};
+				case HttpStatusCode.TooManyRequests:
+					return new ResponseDto {Message = "Too Many Requests", IsSuccess = false};
+				case HttpStatusCode.RequestHeaderFieldsTooLarge:
+					return new ResponseDto {Message = "Request Header Fields Too Large", IsSuccess = false};
+				case HttpStatusCode.UnavailableForLegalReasons:
+					return new ResponseDto {Message = "Unavailable For Legal Reasons", IsSuccess = false};
 				case HttpStatusCode.InternalServerError:
 					return new ResponseDto {Message = "Internal Server Error", IsSuccess = false};
+				case HttpStatusCode.NotImplemented:
+					return new ResponseDto {Message = "Not Implemented", IsSuccess = false};
+				case HttpStatusCode.BadGateway:
+					return new ResponseDto {Message = "Bad Gateway", IsSuccess = false};
+				case HttpStatusCode.ServiceUnavailable:
+					return new ResponseDto {Message = "Service Unavailable", IsSuccess = false};
+				case HttpStatusCode.GatewayTimeout:
+					return new ResponseDto {Message = "Gateway Timeout", IsSuccess = false};
+				case HttpStatusCode.HttpVersionNotSupported:
+					return new ResponseDto {Message = "HTTP Version Not Supported", IsSuccess = false};
+				case HttpStatusCode.VariantAlsoNegotiates:
+					return new ResponseDto {Message = "Variant Also Negotiates", IsSuccess = false};
+				case HttpStatusCode.InsufficientStorage:
+					return new ResponseDto {Message = "Insufficient Storage", IsSuccess = false};
+				case HttpStatusCode.LoopDetected:
+					return new ResponseDto {Message = "Loop Detected", IsSuccess = false};
+				case HttpStatusCode.NotExtended:
+					return new ResponseDto {Message = "Not Extended", IsSuccess = false};
+				case HttpStatusCode.NetworkAuthenticationRequired:
+					return new ResponseDto {Message = "Network Authentication Required", IsSuccess = false};
+				case HttpStatusCode.UpgradeRequired:
+				case HttpStatusCode.PreconditionRequired:
+				case HttpStatusCode.Continue:
+				case HttpStatusCode.SwitchingProtocols:
+				case HttpStatusCode.Processing:
+				case HttpStatusCode.EarlyHints:
+				case HttpStatusCode.OK:
+				case HttpStatusCode.Created:
+				case HttpStatusCode.Accepted:
+				case HttpStatusCode.NonAuthoritativeInformation:
+				case HttpStatusCode.NoContent:
+				case HttpStatusCode.ResetContent:
+				case HttpStatusCode.PartialContent:
+				case HttpStatusCode.MultiStatus:
+				case HttpStatusCode.AlreadyReported:
+				case HttpStatusCode.IMUsed:
+				case HttpStatusCode.Ambiguous:
+				case HttpStatusCode.Moved:
+				case HttpStatusCode.Found:
+				case HttpStatusCode.RedirectMethod:
+				case HttpStatusCode.NotModified:
+				case HttpStatusCode.UseProxy:
+				case HttpStatusCode.Unused:
+				case HttpStatusCode.RedirectKeepVerb:
 				default:
 					var apiContent = await apiResponse.Content.ReadAsStringAsync();
 					var apiResponseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
